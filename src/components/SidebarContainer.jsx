7434d4a6a8a0e69eda35e7f4847973c5c78a0b4e
AllanC12 <allandevfront@gmail.com>
2023-03-19 00:43:15 -0300

    sidebar estruturada

---
+++ src/components/SidebarContainer.jsx
@@ -0,0 +1,12 @@
+import ProfileContainer from "./ProfileContainer"
+import styles from "./sass_components/SidebarContainer.module.scss"
+
+const SidebarContainer = () => {
+  return (
+    <aside className={styles.sidebar}>
+      
+    </aside>
+    )
+}
+
+export default SidebarContainer
