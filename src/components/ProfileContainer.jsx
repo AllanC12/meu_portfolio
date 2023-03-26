56fb7f19effe083fb418cc52d33dcc971af1940d
AllanC12 <allandevfront@gmail.com>
2023-03-26 16:36:21 -0300

    Preenchendo informações de contato

--- src/components/ProfileContainer.jsx
+++ src/components/ProfileContainer.jsx
@@ -2,10 +2,11 @@ import "./sass_components/ProfileContainer.scss";
 
 import {FaLinkedinIn,FaInstagram,FaGithub} from 'react-icons/fa';
 
+ 
 const socialMedias = [
-    {id:'linkedin',icon:<FaLinkedinIn/>},
-    {id:'instagram',icon:<FaInstagram/>},
-    {id:'github',icon:<FaGithub/>}
+    {id:'linkedin',icon:<FaLinkedinIn/>,link:"https://linkedin.com/in/allan-cândido-8ba476260"},
+    {id:'instagram',icon:<FaInstagram/>,link:"https://www.instagram.com/allancandido78/?next=%2F"},
+    {id:'github',icon:<FaGithub/>,link:"https://github.com/AllanC12"}
 ]
 
 const ProfileContainer = () => {
@@ -17,7 +18,12 @@ const ProfileContainer = () => {
         <p className="title-developer">Desenvolvedor Front-End</p>
         <div className="social-medias">
           {socialMedias.map(socialMedia => (
-             <a className={socialMedia.id} key={socialMedia.id} href="">{socialMedia.icon}</a>
+             <a className={socialMedia.id} 
+             target="_blank"
+               key={socialMedia.id} 
+               href={socialMedia.link}>
+               {socialMedia.icon}
+             </a>
           ))}
         </div>
 
